1ede84000b1c2823d8e5308d9a2ba60b956db5f2
Prakul <prakulagrawal.2003@gmail.com>
2023-04-22 20:47:31 +0530

    Changed "authorization" to "x-auth-token"

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     
-    const token = req.header('authorization');
+    const token = req.header('x-auth-token');
     
     if (!token) {
         return res.status(401).json({ error: 'No token, Authentication denied' });
